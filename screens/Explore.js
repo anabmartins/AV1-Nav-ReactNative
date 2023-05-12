94975b2df82e4b891566d84b163580076f98ea74
ANA BEATRIZ MARTINS <ana_beatriz-martins@estudante.sesisenai.org.br>
2023-05-12 08:30:12 -0300

    att screens

--- screens/Explore.js
+++ screens/Explore.js
@@ -1,10 +1,83 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
-export default function TelaB() {
+export default function Issues() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Tela B</Text>
+    <View style={styles.container}>
+      {/* Cabeçalho */}
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Notifications</Text>
+
+        {/* Ícones do canto direito */}
+        
+        </View>
+      
+      {/*Conteúdo */}
+      <View style={styles.content}>
+        </View>
+      {/* Barra de navegação */}
+      <View style={styles.navBar}>
+        <View style={styles.navButton}>
+          <Feather name="home" size={30} color="grey" />
+          <Text style={styles.navText}>Home</Text>
+        </View>
+
+        <View style={styles.navButton}>
+          <Feather name="bell" size={30} color="#2890fa" />
+          <Text style={[styles.navText, { color: '#2890fa' }]}>Notifications</Text>
+        </View>
+
+        <View style={styles.navButton}>
+          <Feather name="search" size={30} color="gray" />
+          <Text style={styles.navText}>Explore</Text>
+        </View>
+
+        <View style={styles.navButton}>
+          <Feather name="user" size={30} color="gray" />
+          <Text style={styles.navText}>Profile</Text>
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#17181c',
+    fontFamily: 'Helvetica',
+  },
+  header: {
+    height: 100,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    padding: 20,
+  },
+  headerText: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+    fontFamily: 'Helvetica',
+  },
+  content: {
+    flex: 1,
+  },
+  navBar: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    height: 80,
+    padding: 20,
+    backgroundColor: '#17181c',
+  },
+  navButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  navText: {
+    fontSize: 14,
+    color: 'gray',
+    fontFamily: 'Helvetica',
+  },
+});
